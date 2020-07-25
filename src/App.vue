f09f6bcacbf79556373d9c5f07a7c2deb42ee474
dwardchan <553723991@qq.com>
2020-07-25 16:46:36 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <left-menu/>
+    <!-- <play-music/>
+    <Main/> -->
   </div>
 </template>
 
-<style>
+<script>
+import LeftMenu from './components/content/LeftMenu'
+// import PlayMusic from "./components/content/playmusic/PlayMusic"
+// import Main from './components/content/Main'
+
+export default {
+  name:'App',
+  components:{
+    LeftMenu,
+    // Main,
+    // PlayMusic
+  }
+}
+</script>
+
+<style scoped>
+@import url('./assets/css/base.css');
+@import url('./assets/css/play.css');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+   width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  user-select: none;
+  background: #16181c;
+  color: #fff;
 }
 </style>
